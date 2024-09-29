82494107b871149c56fb9b821ce84d90cdc307b2
Coded-Adil <itsadil2022@gmail.com>
2024-09-29 23:34:18 +0600

    updated links

--- js/main.js
+++ js/main.js
@@ -74,4 +74,8 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}  
+}
+
+document.getElementById('reg').addEventListener('click', function(){
+  window.location.href='./form.html';
+});
